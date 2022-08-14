3ebd231a5714e4de9f6e84e7a48bbf06d4e58a7c
nguyenduy442000 <nguyenduy442000@gmail.com>
2022-08-14 23:13:19 +0700

    gitrmpack

--- admin/src/components/Button/Button.jsx
+++ admin/src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './button.scss'
 const CButton = (props) => {
-  //
+  
   return (
     <div>
     <button className={props.className} onClick={props.onClick}>{props.name}</button>
